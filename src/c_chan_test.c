5b3f5643e8ded111124f2fd14f3952558c2ce1b1
Paul J. Lucas <paul@lucasmail.org>
2026-01-04 06:45:19 -0800

    Updated copyright year.

--- src/c_chan_test.c
+++ src/c_chan_test.c
@@ -2,7 +2,7 @@
 **      c_chan -- Channels Library for C
 **      src/c_chan_test.c
 **
-**      Copyright (C) 2025  Paul J. Lucas
+**      Copyright (C) 2025-2026  Paul J. Lucas
 **
 **      This program is free software: you can redistribute it and/or modify
 **      it under the terms of the GNU General Public License as published by
